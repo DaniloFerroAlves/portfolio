3fbaa23547e7643b2bbd06b74fd7ec30e880dd09
Dan <danilaoferraoalves@gmail.com>
2024-06-28 13:19:41 -0300

    added: page not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+              <h2 className="text-4xl">404 Not Found</h2>
+              <p className="text-lg">Desculpe, a página que você está procurando não foi encontrada.</p>
+    </div>
+  )
+}
+
+export default NotFound
