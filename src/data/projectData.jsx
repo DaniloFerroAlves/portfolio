435a90d0b3823529348a1953687daafbd3113218
Dan <danilaoferraoalves@gmail.com>
2024-06-28 13:19:29 -0300

    fix: caminho da image do projeto

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -1,16 +1,16 @@
 const projects = [
     {
         "id": 1,
-        "image": "./src/assets/projectsimg/expertnotes.png",
+        "image": "/images/projectsimg/expertnotes.png",
         "title": "Expert Notes",
         "description": "Nessa aplicação você tem a possibilidade de anotar suas coisas tanto com texto ou gravação de voz.",
         "techs": ["React", "Typescript", "SpeechRecognitionAPI"],
-        "urllive": "daniloferroalves.github.io/Expert-Notes/",
+        "urllive": "https://daniloferroalves.github.io/Expert-Notes/",
         "urlsource": "https://github.com/DaniloFerroAlves/Expert-Notes"
     },
     {
         "id": 2,
-        "image": "./src/assets/projectsimg/quizjs.png",
+        "image": "/images/projectsimg/quizjs.png",
         "title": "Front-end Quiz",
         "description": "Responda as perguntas e teste o seu conhecimento sobre front-end.",
         "techs": ["Javascript"],
