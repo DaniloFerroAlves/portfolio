3541c16df8baf3d10cfc49c8241b56f85e195e1f
Dan <danilaoferraoalves@gmail.com>
2024-06-28 13:18:30 -0300

    added: adicionado href ao meu link source

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='w-full'>
         <div className='md:w-4/6 m-auto  py-7 flex-wrap md:mt-32 mt-12 text-normal text-gray-500 flex gap-1 justify-center px-4'>
               All Work &copy; 2024. Danilo Ferro Alves. 
-             <a href="" className='flex underline text-gray-200 gap-2 items-center'> Source <FaGithub className='text-white'/></a>
+             <a href="https://github.com/DaniloFerroAlves/portfolio" className='flex underline text-gray-200 gap-2 items-center'> Source <FaGithub className='text-white'/></a>
             
         </div>
       
