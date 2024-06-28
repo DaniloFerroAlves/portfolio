674a344fabdc41b614fb3643eb7517721d3d9960
Dan <danilaoferraoalves@gmail.com>
2024-06-28 13:18:51 -0300

    fix: ajustado o caminho

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -4,7 +4,7 @@ const MyCard = () => {
   return (
     <div className="w-fit  flex flex-col gap-2 text-lg md:mb-0 mb-10">
       <div className="flex md:flex-col md:gap-0 gap-5">
-      <img src="./src/assets/eu.png" alt="" className="md:w-56 w-24 object-contain  mb-4 rounded-full" />
+      <img src="/images/eu.png" alt="" className="md:w-56 w-24 object-contain  mb-4 rounded-full" />
         <div className="flex flex-col justify-evenly text-base">
         <h1 className="md:text-2xl font-semibold ">Danilo Ferro Alves</h1>
         <h2 className="font-semibold text-gray-400">Desenvolvedor Front-end</h2>
